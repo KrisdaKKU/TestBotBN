113999b7e0fcaadd9c9205f490cf956eb1c28ade
KrisdaKKU <66762533+KrisdaKKU@users.noreply.github.com>
2025-05-21 23:31:27 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        # ตรงนี้คุณสามารถเพิ่มการตรวจสอบ username/password ได้
+        return f"Hello, {username}!"
+    return render_template('login.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
